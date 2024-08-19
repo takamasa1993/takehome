28157641654a16982172823b6dacdd3f655d47af
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-08-19 20:52:43 +0000

    Add-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
+resources :posts do
+  resources :comments, only: [:create, :destroy]
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
